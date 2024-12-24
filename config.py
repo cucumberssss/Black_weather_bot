c4586748ef3f0014bf10c5aa6a636b05735fa1d3
egor <be@gmail.com>
2024-12-24 20:51:45 +0300

    Lastovaya

--- config.py
+++
@@ -1,3 +0,0 @@
-TOKEN = '7579319733:AAEgbKuMmuAE_t8-jyF-qPEKK4m-gf5Hw0E'
-WEATHER_API_URL = 'http://dataservice.accuweather.com/forecasts/v1/daily/10day/'
-WEATHER_API_KEY = "2ElTS6kcT2RQy8k923d5Ar4QQsJLK8tM"
